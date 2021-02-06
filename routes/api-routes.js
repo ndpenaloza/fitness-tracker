adbfe2ffa91b0c94c4cc61d6f600126e6c3124a3
NDPenaloza <ndpenaloza@gmail.com>
2021-02-05 22:55:33 -0800

    added wes bos eslint

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
 const models = require('../models/index.js');
 
-
 module.exports = router;
--- server.js
+++ server.js
@@ -5,22 +5,19 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8888;
 const app = express();
 
-
-
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
-  
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // var db = require('./models')
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -28,6 +25,7 @@ app.use(require('./routes/html-routes.js'));
 app.use(require('./routes/api-routes.js'));
 
 app.listen(PORT, () => {
-    console.log('Everything looks fine and dandy! Server listening on PORT: ' + PORT + ' :{D');
-})
-
+  console.log(
+    `Everything looks fine and dandy! Server listening on PORT: ${PORT} :{D`
+  );
+});
