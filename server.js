e598ed04c50b4233122d5585aa2f2b70d278f40b
NDPenaloza <ndpenaloza@gmail.com>
2021-02-05 22:25:14 -0800

    wrote server.js, server listening

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-var PORT = process.env.PORT || 8888;
-var app = express;
+const PORT = process.env.PORT || 8888;
+const app = express();
+
+
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
@@ -14,17 +18,16 @@ mongoose.connect(
   );
   
 
-var db = require('./models')
+// var db = require('./models')
 
-app.request(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/html-routes.js')(app);
-require('./routes/api-routes.js')(app);
+app.use(require('./routes/html-routes.js'));
+app.use(require('./routes/api-routes.js'));
 
-app.listen(PORT, () =>{
-    if (err) throw err;
+app.listen(PORT, () => {
     console.log('Everything looks fine and dandy! Server listening on PORT: ' + PORT + ' :{D');
 })
 
