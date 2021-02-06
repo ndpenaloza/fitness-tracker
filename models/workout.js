6348febb594baaba10f481c38dac309efad64852
NDPenaloza <ndpenaloza@gmail.com>
2021-02-06 00:03:53 -0800

    worked on workout.js in models

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,41 @@
 const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    exercise: [
+        type: {
+            type: String,
+            required: "PLEASE ENTER EXERCISE"
+        },
+        name: {
+            type: String,
+            required: "PLEASE ENTER EXERCISE NAME"
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+
+    ],
+    date: {
+        type: Date,
+        default: () => new Date()
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
